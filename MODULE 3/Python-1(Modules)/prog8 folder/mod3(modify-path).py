b14d53f77170503d3d492077a677e378139231a5
vishnusai1569 <vishnusai1569@gmail.com>
2025-04-27 21:28:36 +0530

    update1

--- MODULE 3/Python-1(Modules)/prog8 folder/mod3(modify-path).py
+++ MODULE 3/Python-1(Modules)/prog8 folder/mod3(modify-path).py
@@ -1,7 +1,7 @@
 # Store  cal.py  in  c:\\sairam  folder  before  the  program  is  executed
 import  sys
 print(len(sys . path)) #  6
-sys . path .  append('c:\\sairam') # Appends  a  new  directory  to  sys.path
+sys . path .  append('//Users//vishnusai//Desktop//VISHNU//PYTHON NOTES//MODULE 3//Python-1(Modules)//prog5 folder') # Appends  a  new  directory  to  sys.path
 print(len(sys . path)) #7
 import  cal  #  Success  :  cal.py  is  found  in  c:\sairam  folder
 print(cal . x)  #  100
--- MODULE 3/Python-4(packages)/prog3 folder/reuse2(import-module).py
+++ MODULE 3/Python-4(packages)/prog3 folder/reuse2(import-module).py
@@ -6,7 +6,7 @@ a = p1 . mod1 . c1()
 a . m1()  #  How  to  call  method  m1()  of  class  c1  in  mod1  of  package  p1
 print()
 print()
-print(p1 . x)  #  How  to  print  variable  'x'  of  __init__  module  in  package  p1
-p1 . f1()  #  How  to  call  function  f1()  of  __init__  module  in  package  p1
+#print(p1 . x)  #  How  to  print  variable  'x'  of  __init__  module  in  package  p1
+#p1 . f1()  #  How  to  call  function  f1()  of  __init__  module  in  package  p1
 a = p1 . c1()
 a . m1()  #  How  to  call  method  m1()  of  class  c1  in   _init_  module  of  package  p1
